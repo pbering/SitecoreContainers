6e2411de9d5a142ac9eb86af0e6a566d134c770d
Per Manniche Bering <pmbering@gmail.com>
2016-01-07 19:29:08 +0100

    First commit

---
+++ containers/sitecore81rev151207/inetpub/New-Website.ps1
@@ -0,0 +1,29 @@
+﻿[CmdLetBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [ValidateScript({Test-Path $_ -PathType "Container"})]
+    [string]$Path,
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Name
+)
+
+Import-Module WebAdministration
+
+# Remove default site and pool
+Remove-Website -Name "Default Web Site" -ErrorAction SilentlyContinue
+Remove-WebAppPool -Name "DefaultAppPool" -ErrorAction SilentlyContinue
+
+# Create pool
+$pool = New-WebAppPool -Name $Name
+$pool.managedPipelineMode = "Integrated"
+$pool.managedRuntimeVersion = "v4.0"
+$pool.enable32BitAppOnWin64 = $true
+$pool.startMode = "AlwaysRunning"
+$pool.processModel.identityType = "LocalSystem"
+$pool | Set-Item -Force
+$pool.Start()
+
+# Create site
+$site = New-Website -Name $Name -ApplicationPool $Name -PhysicalPath $Path -Port 80
+$site.Start()
